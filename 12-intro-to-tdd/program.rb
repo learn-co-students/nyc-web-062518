bf73fadebd4ae248c06f2384c78c3d9634b1f797
Wangtron <evans.wang@flatironschool.com>
2018-07-12 16:57:19 -0400

    Uploaded lecture video

--- 12-intro-to-tdd/program.rb
+++ 12-intro-to-tdd/program.rb
@@ -1,5 +1,9 @@
 require 'pry'
 
 def is_palindrome?(word)
-  # Check if word is a palindrome
+  raise ArgumentError if !word.is_a?(String)
+  return false if word.empty?
+
+  downcased_word = word.downcase.gsub(/\W/, "")
+  downcased_word == downcased_word.reverse
 end
