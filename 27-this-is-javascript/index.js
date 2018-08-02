6e5c09be21a49819d0f3d1784d9f787f22f70f2d
Andrew Cohn <andrewcohn0@gmail.com>
2018-08-02 16:22:02 -0400

    fixing this keyword

--- 27-this-is-javascript/index.js
+++ 27-this-is-javascript/index.js
@@ -1,2 +1,9 @@
 // `this` is the global scope is the `window` object (for the browser)
 console.log(this)
+
+const fixedArrowThis = {
+  whatisThis: function() {
+    return () => this
+  },
+  name: 'fixed this'
+}
