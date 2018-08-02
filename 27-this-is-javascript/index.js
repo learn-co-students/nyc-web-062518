41b84c00f92c891ba30d28a9416d4187a8e0c86a
Andrew Cohn <andrewcohn0@gmail.com>
2018-08-02 17:22:04 -0400

    THE WINDOW OBJECT

--- 27-this-is-javascript/index.js
+++ 27-this-is-javascript/index.js
@@ -1,6 +1,17 @@
 // `this` is the global scope is the `window` object (for the browser)
 console.log(this)
 
+const dolphin = {
+  name: 'murd3rf4ce',
+  talk: function() {
+    console.log(this.name)
+  }
+}
+
+const speak = dolphin.talk
+speak()
+// dolphin.talk()
+
 const fixedArrowThis = {
   whatisThis: function() {
     return () => this
