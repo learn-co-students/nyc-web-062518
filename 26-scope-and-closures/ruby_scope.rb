b3ecd58b36ca993c5b46843a17a5df101b0ecf71
Andrew Cohn <andrewcohn0@gmail.com>
2018-08-06 17:03:59 -0400

    scope and closures

---
+++ 26-scope-and-closures/ruby_scope.rb
@@ -0,0 +1,15 @@
+# scope review in Ruby
+NAME = 'john'.freeze
+
+def say_name
+  puts NAME
+end
+
+say_name
+
+class Person; end
+
+puts flamingo
+flamingo = 'flamingo'
+
+[1, 2, 3].each { |num| puts num }
