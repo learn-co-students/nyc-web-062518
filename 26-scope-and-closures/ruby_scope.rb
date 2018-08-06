9dd6688bc21ee384f41ad7eae828cd17cfc49f61
Andrew Cohn <andrewcohn0@gmail.com>
2018-08-06 17:08:47 -0400

    add notes to ruby_scope file

--- 26-scope-and-closures/ruby_scope.rb
+++ 26-scope-and-closures/ruby_scope.rb
@@ -1,15 +1,17 @@
 # scope review in Ruby
 NAME = 'john'.freeze
+local_name = 'jimmy'
 
 def say_name
-  puts NAME
+  # puts local_name undefined local variable or method
+  puts NAME # i can read a global constant
 end
 
 say_name
 
-class Person; end
+class Person; end # Person is considered to be a constant
 
-puts flamingo
+puts flamingo # cannot attempt to reference a variable before its declared and assigned
 flamingo = 'flamingo'
 
-[1, 2, 3].each { |num| puts num }
+[1, 2, 3].each { |num| puts num } # this block will be run for each element in the array
