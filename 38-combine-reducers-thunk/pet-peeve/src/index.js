0f8d961b0d7d949ba234d211689e36ec0c102f62
Michael Cheng <michael.cheng@flatironschool.com>
2018-09-18 15:41:10 -0400

    planning

--- 38-combine-reducers-thunk/pet-peeve/src/index.js
+++ 38-combine-reducers-thunk/pet-peeve/src/index.js
@@ -6,3 +6,13 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+// What would we do with **just** React.
+//
+// Redux Store's State ==> pets: [], currentPet: {}
+//
+// App
+//  UploadForm <== own local state
+//  PetList
+//    Pet
+//  PetProfile
