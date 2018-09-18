3d68cf9a40b3310db85eb2361aea1ce56d3cb73b
Michael Cheng <michael.cheng@flatironschool.com>
2018-09-18 15:47:52 -0400

    store boilerplate

--- 38-combine-reducers-thunk/pet-peeve/src/index.js
+++ 38-combine-reducers-thunk/pet-peeve/src/index.js
@@ -3,8 +3,32 @@ import ReactDOM from 'react-dom';
 import './assets/css/index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function reducer(state, action) {
+  switch(action.type) {
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+
+
+// Starting: Setup this stuff first!
+// 1. createStore, => 5 ====> need a reducer
+// 2. create a reducer, => 3 ==>
+// 3. Provider, => 1 ===> needs a store
+
+// Worry about this later!
+// actions,
+//  dispatch,
+//  mapStateToProps,
+//  mapDispatchToProps,
+//  connect
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
 
 // What would we do with **just** React.
